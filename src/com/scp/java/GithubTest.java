48c1fd74b1f0019cfbfe1cd4bc955aefde6027ac
BhaktiSP <bhaktipotdar903@gmail.com>
2018-12-15 17:23:32 +0530

    first commit

---
+++ src/com/scp/java/GithubTest.java
@@ -0,0 +1,12 @@
+package com.scp.java;
+
+public class GithubTest {
+	
+		public static void main(String[] args) {
+			System.out.println("hello");
+			System.out.println("hi bhakti");
+		}
+
+	}
+
+
